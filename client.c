35212d11d55cddfbedcf645605d4f5fed3cb5cb2
Cezar Todirisca <cezartodirisca@gmail.com>
2020-12-02 21:32:56 +0200

    Add files via upload

---
+++ client.c
@@ -0,0 +1,93 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int PORT;
+
+extern int errno;
+
+// Initialize a server structure.
+struct sockaddr_in initialize_server(char *ip_address)
+{
+  struct sockaddr_in server;
+
+  bzero(&server, sizeof(server));
+  
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = inet_addr(ip_address);
+  server.sin_port = htons(PORT);
+
+  return server;
+}
+
+void printError(char *message)
+{
+    perror(message);
+    exit(errno);
+}
+
+int main(int argc, char *argv[]) {
+    if(argc != 3)
+    {
+        printf("The syntax is : <server_address> <port>");
+        return -1;
+    }
+
+    PORT = atoi(argv[2]);
+
+    struct sockaddr_in serverStruct = initialize_server(argv[1]);
+    int socketD;
+
+    
+    //Create the socket and check for errors.
+    if( (socketD = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        printError("ERROR AT socket() function.\n");
+    }
+
+    //Connect to the server
+    if(connect(socketD, (struct sockaddr *) &serverStruct, sizeof(struct sockaddr)) == -1)
+    {
+        printError("ERROR AT connect().\n");
+    }
+
+    char message[100];
+
+    while(1)
+    {
+        printf("[Client]Introduceti un mesaj: ");
+        fflush(stdout);
+
+        int msgLength;
+
+        if((msgLength = read(0,message,100)) <= 0)
+        {
+            printError("Nothing to read!");
+        }
+
+        //First write the length of the message
+        if(write(socketD,&msgLength,sizeof(int)) < 0)
+        {
+            printError("Error at writing");
+        }
+        if(write(socketD,message,msgLength) < 0)
+        {
+            printError("Error at writing the message");
+        }
+
+        int newMsgLength;
+        if(read(socketD,&newMsgLength,sizeof(int)) < 0)
+        {
+            printf("Error at reading from server");
+        }
+    }
+
+
+}
--- server.c
+++ server.c
@@ -1,14 +1,32 @@
 #include <arpa/inet.h>
+#include <asm-generic/socket.h>
 #include <errno.h>
 #include <netinet/in.h>
 #include <pthread.h>
 #include <sqlite3.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+const int PORT = 4200;
+socklen_t sockLength = (socklen_t)sizeof(struct sockaddr_in); 
+extern int errno;
+
+void printError(char *message)
+{
+    perror(message);
+    exit(errno);
+}
+
+typedef struct Thread{
+  int threadId; 
+  int clientId;
+}Thread;
 
 // Struct to describe client.
 struct {
@@ -39,4 +57,109 @@ struct {
   float prices[3];
 } news;
 
-int main() { return 0; }
+// Initialize a server structure.
+struct sockaddr_in initialize_server()
+{
+  struct sockaddr_in server;
+
+  bzero(&server, sizeof(server));
+  
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = htonl(INADDR_ANY);
+  server.sin_port = htons(PORT);
+
+  return server;
+}
+
+void set_socket(int *socketD)
+{
+  if((*socketD = socket(AF_INET,SOCK_STREAM,0)) == -1)
+  {
+    printError("Error at creating the socket");
+  }
+  setsockopt(*socketD, SOL_SOCKET, SO_REUSEADDR, (const void*)1, sizeof(int));
+}
+
+int interact(Thread *thread)
+{
+  int msgLength;
+  if(read(thread->clientId,&msgLength,sizeof(int)) <= 0){
+    perror("Read() error in thread");
+    return 0;
+  }
+  
+  char *message;
+  message = (char*)malloc(msgLength+1);
+
+  if(read(thread->clientId,message,msgLength) <= 0){
+    perror("Error at reading of the message in thread");
+    return 0;
+  }
+  message[msgLength]='\0';
+
+  int newLength = strlen(message);
+  if(write(thread->clientId,&newLength,sizeof(int)) <= 0)
+    perror("Error at write in thread");
+
+  printf("Sent back to the client: %d\n",newLength);
+  return 1;
+}
+
+void lobby(Thread *thread)
+{
+  printf("[Thread %d] - Wait for the message: \n",thread->threadId);
+  
+  fflush(stdout);
+  pthread_detach(pthread_self());
+  while(1)
+  {
+    int answer = interact(thread);
+    if(!answer)break;
+  }
+  close((intptr_t)thread);
+}
+
+
+
+int main() {
+  struct sockaddr_in serverStruct = initialize_server();  
+  struct sockaddr_in clientStruct;
+
+  int socketD;
+  pthread_t threads[100];
+
+  set_socket(&socketD);
+
+  //Bind the socket.
+  if(bind(socketD,(struct sockaddr*)&serverStruct,sizeof(struct sockaddr)) == -1)
+    printError("Error at binding the socket");
+  
+  //Make the server listen for the clients.
+  if(listen(socketD,5) == -1)
+    printError("[Server] Error at listen()");
+
+  int index = 0;
+
+  //Wait for the clients to connect and then serve them.
+  while(1)
+  {
+    int clientId;
+    Thread *currThread;
+    
+    printf("[Server] Waiting at port %d \n",PORT);
+    //Accept a client in blocking way.
+    if(( clientId = accept(socketD,(struct sockaddr*) &clientStruct,&sockLength)) < 0)
+    {
+        perror("[Server]Accept error\n");
+        continue;
+    }
+
+    currThread = (struct Thread*)malloc(sizeof(struct Thread));
+    currThread->threadId = index++ ;
+    currThread->clientId = clientId;
+
+    pthread_create(&threads[index],NULL,(void*)&lobby, currThread);
+  }
+
+  return 0; 
+}
