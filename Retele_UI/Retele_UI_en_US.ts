d4d374f38413dad8486d61d67e3f3947dccd4028
Cezar Todirisca <cezartodirisca@gmail.com>
2020-12-13 01:38:44 +0200

    Added first try to build UI

---
+++ Retele_UI/Retele_UI_en_US.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="Retele_UI_en_US">
+<context>
+    <name>client</name>
+    <message>
+        <location filename="client.ui" line="20"/>
+        <source>Monitorizarea Traficului UI</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="client.ui" line="69"/>
+        <source>LOGIN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="client.ui" line="108"/>
+        <source>Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="client.ui" line="195"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="client.ui" line="280"/>
+        <source>New account?Register!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
